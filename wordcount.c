ab66f4df3fc61a5104d0920f3892e6081add1825
DIVYA S <divyasundarraj1996@gmail.com>
2016-07-08 21:00:26 +0530

    Create wordcount.c

---
+++ wordcount.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[100];
+	int i,l,c=1;
+	scanf("%[^\n]s",a);
+	l=strlen(a);
+	for(i=0;i<l;i++)
+	{
+	//	if(a[i]>=65&&a[i]<=90||a[i]>=97&&a[i]<=122||a[i]>=48&&a[i]<=57)
+	//		c++;
+		 if(a[i]==' ')
+			c=c+1;
+	}
+	printf("%d",c);
+	return 0;
+}
